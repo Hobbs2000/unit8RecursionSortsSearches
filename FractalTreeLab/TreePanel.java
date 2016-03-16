221d11f7614c1f8cc3da63ef825e491c9d83c014
Hobbs2000 <hobbs2018@gmail.com>
2016-03-15 23:48:52 -0500

    made a working fractal tree

--- FractalTreeLab/TreePanel.java
+++ FractalTreeLab/TreePanel.java
@@ -5,8 +5,8 @@ import java.awt.Dimension;
 
 public class TreePanel extends JPanel
 {
-    private final int WIDTH = 550;
-    private final int HEIGHT = 550;
+    private final int WIDTH = 1200;
+    private final int HEIGHT = 1000;
     
     private int order = 1;
     private int topX, topY;
@@ -25,19 +25,23 @@ public class TreePanel extends JPanel
         setPreferredSize(new Dimension(WIDTH, HEIGHT));
     }
     
+    public void setOrder(int newOrder)
+    {
+        order = newOrder;
+    }
+    
     public void paintComponent(Graphics g)
     {
         
         g.setColor(Color.RED);
         
-        draw(g, order, topX, topY,90,10);
+        draw(g, order, topX, topY,90,20, 200.00);
     }
     
     //public void draw(Graphics g, int order, int topX, int topY, int botX, int botY, double angle)
-    public void draw(Graphics g, int order, int x1, int y1, double angle, int adder) 
+    public void draw(Graphics g, int order, int x1, int y1, double angle, int adder, double length) 
     {
         int deltaX, deltaY, x2, y2, x3, y3; 
-        double length;
         //double degrees = angle + 10;
         double newAngle = Math.toRadians(angle); //Get 30 degrees in radians
         
@@ -47,26 +51,19 @@ public class TreePanel extends JPanel
         }
         else
         {
-            
-            length = 10;
+           
             
             //x2 = (int)Math.abs(Math.round(Math.sin(newAngle) * length) - topX);
-            x2 = (x1 - (int)Math.round(Math.cos(newAngle) * order * 5));
+            x2 = (x1 - (int)Math.round(Math.cos(newAngle) * order * 10));
+            System.out.println("x:"+x2);
             //y2 = (int)Math.abs(Math.round(Math.cos(newAngle) * length) - topY);
-            y2 = (y1 - (int)Math.round(Math.sin(newAngle) * order * 5));
+            y2 = (y1 - (int)Math.round(Math.sin(newAngle) * order * 10));
+            System.out.println("y:"+y2);
             
             g.drawLine(x1, y1, x2, y2);
             
-            if ((x2 % 7) == 0)
-            {
-                g.setColor(Color.GREEN);
-            }
-            else
-            {
-                g.setColor(Color.BLACK);
-            }
-            draw(g, order-1, x2, y2, angle-adder, adder+2);
-            draw(g, order-1, x2, y2, angle+adder, adder+2);
+            draw(g, order-1, x2, y2, angle+30, adder+2, length+20);
+            draw(g, order-1, x2, y2, angle-30, adder+2, length+20);
         }
     }
 }
--- FractalTreeLab/TreeViewer.java
+++ FractalTreeLab/TreeViewer.java
@@ -1,11 +1,21 @@
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Color;
 
 public class TreeViewer extends JFrame
 {
     private final int HEIGHT = 1080;
     private final int WIDTH = 1900;
     
+    private int order = 1;
+    
     private TreePanel drawer;
+    private JPanel holder;
+    private JButton increase;
+    private JButton decrease;
     
     public TreeViewer()
     {
@@ -13,8 +23,20 @@ public class TreeViewer extends JFrame
         this.setSize(WIDTH, HEIGHT);
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         
-        drawer = new TreePanel(20, 700, 900, 700, 900);
-        this.add(drawer);
+        holder = new JPanel();
+        
+        increase = new JButton("Increase");
+        increase.addActionListener(new ClickListener());
+        holder.add(increase);
+        
+        decrease = new JButton("Decrease");
+        decrease.addActionListener(new ClickListener());
+        holder.add(decrease);
+        
+        drawer = new TreePanel(order, 700, 1000, 700, 1000);
+        holder.add(drawer);
+        
+        this.add(holder);
         
         this.setVisible(true);
     }
@@ -22,6 +44,27 @@ public class TreeViewer extends JFrame
     public static void main(String[] args)
     {
         JFrame frame = new TreeViewer();
+        frame.setBackground(Color.WHITE);
         frame.repaint();
     }
+    
+    public class ClickListener implements ActionListener
+    {
+        public void actionPerformed(ActionEvent event)
+        {
+            if (event.getSource() == increase)
+            {
+                order++;
+                drawer.setOrder(order);
+                repaint();
+            }
+            else
+            {
+                order--;
+                drawer.setOrder(order);
+                repaint();
+            }
+        }
+    }
+    
 }
